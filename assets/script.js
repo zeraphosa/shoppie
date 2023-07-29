bc7bf22bdf5e3a1a44d5422ee3c0e62f1aec49ed
zeraphosa <zaursharifov@outlook.com>
2023-07-29 19:10:22 +0400

    header done

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,12 @@
 const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
-navToggler.addEventListener('click', function(){
-    navbar.classList.toggle('active')
-})
+navToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+});
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[this.scrollY > 50 ? "add" : "remove"]("active");
+});
